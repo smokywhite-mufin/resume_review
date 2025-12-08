99eac85b0759e9b4e2e15fac8ba85a5f647ee90d
smokywhite-mufin <smokywhite@must.company>
2025-12-08 09:54:03 +0900

    chore: add multer

--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const multer = require('multer');
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
 
 const app = express();
 const PORT = 8000;
 
 app.use(cors());
 app.use(express.json());
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+const upload = multer({ storage: storage });
 
 app.get('/', (req, res) => {
     res.send('Hello, Node.js!');
